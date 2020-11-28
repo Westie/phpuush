d7a4633b5e79063b59fdee6f11541e7006abe86e
David Weston <westie@typefish.co.uk>
2020-11-28 02:55:33 +0000

    Added proper caching

--- app/src/Router/FileRoute.php
+++ app/src/Router/FileRoute.php
@@ -30,14 +30,18 @@ class FileRoute
 
         $file = $fileRepository->getFileByAlias($args['alias']);
 
+        if ($request->getHeaderLine('If-None-Match') === $file['file_hash']) {
+            return $response->withStatus(304);
+        }
+
         return $response->withStatus(200)
             ->withHeader('Cache-Control', 'public')
-            ->withHeader('Last-Modified', date('r', $file['timestamp']))
-            ->withHeader('Content-Length', $file['file_size'])
-            ->withHeader('Content-Type', $file['mime_type'])
-            ->withHeader('Content-Transfer-Encoding', 'binary')
-            ->withHeader('Content-MD5', $file['file_hash'])
             ->withHeader('Content-Disposition', 'inline; filename=' . json_encode($file['file_name']))
+            ->withHeader('Content-Length', $file['file_size'])
+            ->withHeader('Content-MD5', $file['file_hash'])
+            ->withHeader('Content-Transfer-Encoding', 'binary')
+            ->withHeader('Content-Type', $file['mime_type'])
+            ->withHeader('ETag', $file['file_hash'])
             ->withBody(new Stream($file['file_stream']));
     }
 }
